4c582a33baf99f1cc03dae651537efb3f90759da
Vladimir <123073829+TykiJam@users.noreply.github.com>
2023-08-16 16:16:38 +0300

    Add files via upload

---
+++ homework1/script.js
@@ -0,0 +1,13 @@
+const batN = document.querySelector('#btn-test');
+const icon1 = document.querySelector('#svg1')
+const icon2 = document.querySelector('#svg2')
+
+batN.addEventListener('click', () => {
+    
+    icon1.classList.toggle('hidde');
+    icon2.classList.toggle('hidde');
+    
+
+      
+    
+});
